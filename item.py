c162b24db3881f3b879fe6dd4ca8d191b4133322
olu sogbesan <sogbesano@gmail.com>
2016-02-01 14:03:00 +0100

    getitem and len methods added to the ItemList class

--- item.py
+++ item.py
@@ -23,3 +23,16 @@ class ItemList(object):
     '''given a key returns the associated item'''
     return self.items[key]
 
+  def get_all_on_items(self, chromosome, m):
+    '''returns on items with their associated knapsacks as dict of lists'''
+    all_on_items = {}
+    on_items = []
+    for knapsack in range(1, m + 1):
+       for gene in range(1, len(chromosome), 2):
+         if chromosome[gene] == str(knapsack):
+           index = 0 if gene == 1 else ((gene - 1) / 2)
+           on_items.append(self.items[int(index)])
+       all_on_items[knapsack] = on_items
+       on_items = []
+    return all_on_items
+
