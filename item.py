51a67cd491e5d2f5fb4af4135d1a594f9b915bdb
olu sogbesan <sogbesano@gmail.com>
2016-02-01 13:08:00 +0100

    ItemList class created

--- item.py
+++ item.py
@@ -7,3 +7,12 @@ class Item(object):
 
     def __str__(self):
     return 'i = {0}, p = {1}, w = {2}'.format(self.index, self.profit, self.weight)
+
+class ItemList(object):
+
+  def __init__(self, items):
+    self.items = items
+
+  def __str__(self):
+    return '\n'.join([str(item) for item in self.items])
+
