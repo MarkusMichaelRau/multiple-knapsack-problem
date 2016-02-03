1e2f500fa14c4989a28c11dfb0e8ed163d30ee3d
olu sogbesan <sogbesano@gmail.com>
2016-02-03 15:13:00 +0100

    chromosome class has been created

---
+++ chromosome.py
@@ -0,0 +1,13 @@
+from util import gen_item_list, iseven
+
+class Chromosome(object):
+
+  def __init__(self, solution=None):
+    self.solution = solution
+  
+  def __str__(self):
+    return self.solution
+
+    
+         
+
