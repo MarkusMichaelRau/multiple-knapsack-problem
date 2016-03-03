838445c61e7134dc8bb987420704a62734ddd41d
olu sogbesan <sogbesano@gmail.com>
2016-03-03 11:48:00 +0100

    sum_item_attribute method added to the FitnessFunction class

--- fitness.py
+++ fitness.py
@@ -5,3 +5,10 @@ class FitnessFunction(object):
   def __init__(self, chromosome, knapsack_items_map):
     self.chromosome = chromosome
     self.knapsack_items_map = knapsack_items_map
+
+  def sum_item_attribute(self, m, attribute):
+    '''returns the sum of specified attribute of items in knapsacks m'''
+    return sum([item.profit if attribute == 'profit' else item.weight for item in self.knapsack_items_map[m]])
+
+
+
