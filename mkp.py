07fd0951fd8d9d2d2d6b66a19dca3b245c2698cc
olu sogbesan <sogbesano@gmail.com>
2016-03-07 12:13:00 +0100

    calculating population sum and selection applied in mkp file

--- mkp.py
+++ mkp.py
@@ -23,5 +23,29 @@ if __name__ == '__main__':
   print('number of knapsacks = {0}'.format(m))
   print('number of items = {0}'.format(n))
   
- 
+  p = 10
+  print('population size = ' + str(p))
+  population = PopulationFactory(p, n, m).gen()
+  iterations = 100
+  fittest_chromosome = None
+  fittest_chromosomes = []
+  for _ in range(iterations):
+    pop_fsum = 0
+    #calc population sum
+    for i in range(p):
+      fitness_function = FitnessFunction(population[i], items.get_all_on_items(population[i].solution, m))
+      pop_fsum = pop_fsum + fitness_function.sum_all_fitness()
+      population[i] = fitness_function.chromosome
+
+    parents = []
+    for i in range(2):#select 2 chromosomes for crossover
+      parent = RouletteSelection(population).do_selection()
+      parents.append(parent)
+
+    point = choice(range(0, len(parents[0]), 2))
+
+    children = OnePoint().exe(parents[0], parents[1], point)
+
+    probability = 100
+    mutated_children = []
   
